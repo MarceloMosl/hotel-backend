78cc834bd2d79cd4aacd6e24ee483df532824c9d
MarceloMosl <marcelomosl@icloud.com>
2023-06-04 20:05:29 -0300

    Feat: sign-in rota post '/'

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Express } from "express";
 import cors from "cors";
 import { connectDb, disconnectDB } from "./config";
-
+import { usersRouter } from "./routes/users-route";
 const app = express();
 
-app.use(cors());
-
-app.get("/health", (_req, res) => res.send("OK!"));
+app
+  .use(cors())
+  .use(express.json())
+  .get("/health", (_req, res) => res.send("OK!"))
+  .use("/", usersRouter);
 
 export function init(): Promise<Express> {
   connectDb();
---
+++ src/repositories/user-repository/index.ts
@@ -0,0 +1,21 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/config";
+
+async function findByEmail(email: string) {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+}
+
+async function create(data: Prisma.UserUncheckedCreateInput) {
+  return prisma.user.create({
+    data,
+  });
+}
+
+const userRepository = {
+  findByEmail,
+  create,
+};
+
+export default userRepository;
